fb86fa99ac469a3824600ee1756d5701a660f720
Oscar Campos <oc.darkreaper@gmail.com>
2021-04-08 16:07:37 +0100

    new resources and debugbar

--- luizaandaluz/app/Providers/RouteServiceProvider.php
+++ luizaandaluz/app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\Http\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -44,7 +44,6 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->namespace($this->namespace)
-                ->namespace('App\Http\Controllers')
                 ->group(base_path('routes/web.php'));
         });
     }
