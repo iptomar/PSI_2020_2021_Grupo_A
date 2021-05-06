12e74d7d97d6c7cecefce5124663d6e56c81d282
Thanos <oc.darkreaper@gmail.com>
2021-05-06 16:06:01 +0100

    translations

---
+++ luizaandaluz/resources/lang/pt/frontoffice.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'home' => 'Início',
+    'foundation' => 'Fundação',
+    'contact' => 'Contato',
+
+    'formCreate' => 'Criar Interação',
+    'formTitle' => 'Título',
+    'formDescription' => 'Descrição',
+
+];
