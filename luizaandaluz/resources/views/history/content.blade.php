b3ef8a2af1ba86c3b2983e445802852e9f541e14
Diogo Polidoro <aluno20752@ipt.pt>
2021-05-10 21:46:05 +0100

    Informação

---
+++ luizaandaluz/resources/views/history/content.blade.php
@@ -0,0 +1,6 @@
+<div class="wrapper">
+    <div class="row text-center">
+    <span class="font-weight-bold">@lang('frontoffice.titleHistory')</span>
+    </div>
+</div>
+
--- luizaandaluz/resources/views/history/index.blade.php
+++ luizaandaluz/resources/views/history/index.blade.php
@@ -2,21 +2,7 @@
 
 @section('title', lang('fullstack.title'))
 
-@section('css')
-<link rel="stylesheet" href="/css/map.css">
-@endsection
-
 @section('content')
-
+@include('history.content')
 @endsection
 
-@section('scripts')
-<script src="/js/map.js"></script>
-@if(isset($message))
-        <script>
-            var json_messages = {!! $message !!}
-        </script>
-@endif
-
-<script src="/js/toast.js"></script>
-@endsection
