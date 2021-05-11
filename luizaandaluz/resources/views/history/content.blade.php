1dc73047915c2b26be018961609338bf23f2d4d4
Diogo Polidoro <aluno20752@ipt.pt>
2021-05-11 17:00:24 +0100

    History update

--- luizaandaluz/resources/views/history/content.blade.php
+++ luizaandaluz/resources/views/history/content.blade.php
@@ -1,6 +1,13 @@
-<div class="wrapper">
-    <div class="row text-center">
-    <span class="font-weight-bold">@lang('frontoffice.titleHistory')</span>
+<div class="brand-page brand-img">
+    <div>
+        <p class="brand-titulo text-center"><br>@lang('frontoffice.titleHistory')</p>
+    </div>
+    <div>
+        <br><p class="brand-texto text-left">@lang('frontoffice.infHistory')</p>
+        <p class="brand-texto text-left">@lang('frontoffice.infHistory2')</p>
+        <p class="brand-texto text-left">@lang('frontoffice.infHistory3')</p>
     </div>
 </div>
+
+
 
