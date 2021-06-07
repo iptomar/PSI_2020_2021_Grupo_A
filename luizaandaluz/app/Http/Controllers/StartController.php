63c66c2d538eec45b773b1a26ff7fcc7490cddc2
Thanos <oc.darkreaper@gmail.com>
2021-06-07 21:31:53 +0100

    criação da pagina de grupos

--- luizaandaluz/app/Http/Controllers/StartController.php
+++ luizaandaluz/app/Http/Controllers/StartController.php
@@ -11,6 +11,112 @@ class StartController extends Controller
     }
 
     public function group(){
-        return view('group.index');
+        $alunos = $this->getInfo();
+        return view('group.index')->with('alunos',$alunos);
+    }
+
+    private function getInfo(){
+        return [
+            $this->andre(),
+            $this->camilo(),
+            $this->diogo(),
+            $this->flavio(),
+            $this->jessica(),
+            $this->oscar(),
+            $this->pedro(),
+            $this->samuel(),
+        ];
+    }
+
+    private function camilo(){
+        return [
+            'name'=>'',
+            'website'=>['url'=>'','name'=>''],
+            'image'=>'',
+            'birthday'=>'',
+            'desc'=>lang('group.camilo'),
+            'number'=>'',
+        ];
+    }
+
+    private function diogo(){
+        return [
+            'name'=>'',
+            'website'=>['url'=>'','name'=>''],
+            'image'=>'',
+            'birthday'=>'',
+            'desc'=>lang('group.diogo'),
+            'number'=>'',
+        ];
+    }
+
+    private function oscar(){
+        return [
+            'name'=>'Óscar Arcizet Campos',
+            'website'=>['url'=>'https:\\cv-oscar.unicaos.eu','name'=>'Portfolio'],
+            'image'=>'',
+            'birthday'=>'1989-04-19',
+            'desc'=>lang('group.oscar'),
+            'number'=>'21997',
+        ];
+    }
+
+    private function andre(){
+        return [
+            'name'=>'',
+            'website'=>['url'=>'','name'=>''],
+            'image'=>'',
+            'birthday'=>'',
+            'desc'=>lang('group.andre'),
+            'number'=>'',
+        ];
+    }
+
+    private function samuel(){
+        return [
+            'name'=>'',
+            'website'=>['url'=>'','name'=>''],
+            'image'=>'',
+            'birthday'=>'',
+            'desc'=>lang('group.samuel'),
+            'number'=>'',
+        ];
+    }
+
+    private function pedro(){
+        return [
+            'name'=>'',
+            'website'=>['url'=>'','name'=>''],
+            'image'=>'',
+            'birthday'=>'',
+            'desc'=>lang('group.pedro'),
+            'number'=>'',
+        ];
+    }
+
+    private function jessica(){
+        return [
+            'name'=>'',
+            'website'=>['url'=>'','name'=>''],
+            'image'=>'',
+            'birthday'=>'',
+            'desc'=>lang('group.jessica'),
+            'number'=>'',
+        ];
+    }
+
+    private function flavio(){
+        return [
+            'name'=>'',
+            'website'=>['url'=>'','name'=>''],
+            'image'=>'',
+            'birthday'=>'',
+            'desc'=>lang('group.flavio'),
+            'number'=>'',
+        ];
+    }
+
+    private function professor(){
+
     }
 }
--- luizaandaluz/resources/views/group/index.blade.php
+++ luizaandaluz/resources/views/group/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layout.neutral')
 
 @section('title', lang('fullstack.title'))
+@section('css')
+    <style>
+        .mb-6{
+            margin-bottom: 5rem!important;
+        }
+    </style>
+@endsection
 
 @section('content')
 @include('group.content')
