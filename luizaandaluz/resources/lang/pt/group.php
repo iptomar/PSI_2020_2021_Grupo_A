1677f3edcea246e3ec88fd338e395a3b6a9d25a4
Andre Ramos <aluno20710@ipt.pt>
2021-06-17 11:11:47 +0100

    group

--- luizaandaluz/resources/lang/pt/group.php
+++ luizaandaluz/resources/lang/pt/group.php
@@ -2,13 +2,8 @@
 
 return [
     'oscar'=>'Não existe muito a dizer, tenho vários hobbies mas sou claramente um gamer e programador.',
-<<<<<<< HEAD
-    'andre'=>'Sou um aluno de Engenharia Informática, com 21 anos e muito gosto por informática',
-    'camilo'=>'',
-=======
-    'andre'=>'',
+    'andre'=>'Sou um rapaz que gosta bastante de tecnologia e de se divertir nos tempos livres.',
     'camilo'=>'Sinceramente, não concordo nem discordo... muito pelo contrário.',
->>>>>>> 98f1a7ba8490723701da731303084177b7d66d16
     'flavio'=>'',
     'jessica'=>'Sou aluna do terceiro ano de Licenciatura de Engenharia Informática, no Instituto Politécnico de Tomar.',
     'diogo'=>'Sou apenas um rapazito que tem como part-time jogar computador e sair com os amigos.',
