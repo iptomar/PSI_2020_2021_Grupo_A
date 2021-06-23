9ca7be3ff68561dd26fc68ea0fff88f553fa3339
Thanos <oc.darkreaper@gmail.com>
2021-06-23 16:07:55 +0100

    atualizar mapa

--- luizaandaluz/public/js/map.js
+++ luizaandaluz/public/js/map.js
@@ -194,8 +194,6 @@ $('#addvideo').on('click',function(){
     code += '</div>';
     code += '</div>';
     $('#videos').append(code);
-    getVideos();
-
 });
 
 $('#addlink').on('click',function(){
