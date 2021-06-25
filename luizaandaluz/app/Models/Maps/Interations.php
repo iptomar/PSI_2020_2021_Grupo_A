bfd25512e8f97f598cc9649722ad8f5124c2da54
Camilo Sauane <aluno21085@ipt.pt>
2021-06-25 01:24:23 +0100

    Correção no model das Interations

--- luizaandaluz/app/Models/Maps/Interations.php
+++ luizaandaluz/app/Models/Maps/Interations.php
@@ -1,123 +1,52 @@
 <?php
 
-
-
 namespace App\Models\Maps;
-
-
 
 use Jenssegers\Mongodb\Eloquent\Model;
 use Ramsey\Uuid\Uuid;
 
-
-
 class Interations extends Model
 {
-protected $collection = 'map_interation';
+    protected $collection = 'map_interation';
+
+
+    protected $primaryKey = 'uuid';
 
 
 
-protected $primaryKey = 'uuid';
+    protected $fillable = [
+    'uuid', 'location','name','email','birthday','title','description','active'
+    ];
 
 
 
-protected $fillable = [
-'uuid', 'location','name','email','birthday','title','description','active'
-];
+    public $visible = [
+    'uuid', 'location','name','email','birthday','title','description','active'
+    ];
 
 
 
-public $visible = [
-'uuid', 'location','name','email','birthday','title','description','active'
-];
-
-
-
-public static function getInteration(){
-return Interations::firstorNew([
-'uuid' => Uuid::uuid1()->toString(),
-'location' => null,
-'name' => null,
-'email' => null,
-'birthday' => null,
-'title' => null,
-'description' => null,
-'active' => 0,
-]);
-}
-
-
+    public static function getInteration(){
+        return Interations::firstorNew([
+            'uuid' => Uuid::uuid1()->toString(),
+            'location' => null,
+            'name' => null,
+            'email' => null,
+            'birthday' => null,
+            'title' => null,
+            'description' => null,
+            'active' => 0,
+        ]);
+    }
 
 public function locations()
 {
-return $this->belongsTo(Locations::class,'location','uuid');
+    return $this->belongsTo(Locations::class,'location','uuid');
 }
-
-
 
 public function files()
 {
-return $this->hasMany(Files::class,'uuid');
-}
-}<?php
-
-
-
-namespace App\Models\Maps;
-
-
-
-use Jenssegers\Mongodb\Eloquent\Model;
-use Ramsey\Uuid\Uuid;
-
-
-
-class Interations extends Model
-{
-protected $collection = 'map_interation';
-
-
-
-protected $primaryKey = 'uuid';
-
-
-
-protected $fillable = [
-'uuid', 'location','name','email','birthday','title','description','active'
-];
-
-
-
-public $visible = [
-'uuid', 'location','name','email','birthday','title','description','active'
-];
-
-
-
-public static function getInteration(){
-return Interations::firstorNew([
-'uuid' => Uuid::uuid1()->toString(),
-'location' => null,
-'name' => null,
-'email' => null,
-'birthday' => null,
-'title' => null,
-'description' => null,
-'active' => 0,
-]);
+    return $this->hasMany(Files::class,'uuid');
 }
 
-
-
-public function locations()
-{
-return $this->belongsTo(Locations::class,'location','uuid');
-}
-
-
-
-public function files()
-{
-return $this->hasMany(Files::class,'uuid');
-}
 }
